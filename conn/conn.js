ec6be99df71775f8927c90d807fbfe073656f994
Gabriel Matos <g.matos@outlook.com>
2021-12-17 22:52:15 -0300

    ajuste na formatação do conn.js

--- conn/conn.js
+++ conn/conn.js
@@ -1,9 +1,7 @@
 const mongoose = require('mongoose');
 
 const Conn = () => {
-  mongoose.connect('mongodb://localhost:27017/tasks', {
-    useNewUrlParser: true
-  })
+  mongoose.connect('mongodb://localhost:27017/tasks', { useNewUrlParser: true })
   .then(() => console.log('Conexão com MongoDB executada com sucesso!'))
   .catch((err) => console.error(`Erro na conexão com o banco: ${err}`));
 }
