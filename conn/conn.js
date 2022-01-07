830820e1b6c31d7596c32970728b5e94fef00f56
Gabriel Matos <g.matos@outlook.com>
2022-01-07 19:59:27 -0300

    conecta Atlas

--- conn/conn.js
+++ conn/conn.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const Conn = () => {
-  mongoose.connect('mongodb://localhost:27017/tasks', { useNewUrlParser: true })
+  mongoose.connect('mongodb+srv://dbUser:XeNQFv3nge3Sso0D@todolist.jvyvs.mongodb.net/toDoList?retryWrites=true&w=majority', { useNewUrlParser: true })
   .then(() => console.log('Conexão com MongoDB executada com sucesso!'))
   .catch((err) => console.error(`Erro na conexão com o banco: ${err}`));
 }
