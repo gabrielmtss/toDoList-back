bdf95798fee941be452745731a42c8e9c68730bc
Gabriel Matos <g.matos@outlook.com>
2022-01-10 19:32:52 -0300

    configura rotas no index e corrige app.listen

--- index.js
+++ index.js
@@ -5,11 +5,14 @@ if(process.env.NODE_ENV !== 'production') {
 const express = require('express');
 const cors = require('cors');
 const Conn = require('./conn/conn');
+const tasksRouter = require('./routes/tasks.routes');
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.use('/tasks', tasksRouter);
 
 const db_url = process.env.DB_URL;
 const db_user = process.env.DB_USER;
@@ -19,6 +22,6 @@ const db_data = process.env.DB_DATA;
 Conn(db_url, db_user, db_pass, db_data);
 
 const port = 3000;
-app.listen(process.env.PORT | port, () => {
+app.listen(process.env.PORT || port, () => {
   console.log(`Servidor inicializado na porta ${port}`);
 });
