8d195f95cfb096343ff7d578d7cd632bb57a9ccd
Gabriel Matos <g.matos@outlook.com>
2022-01-09 23:55:24 -0300

    configura conexão com atlas

--- conn/conn.js
+++ conn/conn.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose');
 
-const Conn = () => {
-  mongoose.connect('mongodb+srv://dbUser:XeNQFv3nge3Sso0D@todolist.jvyvs.mongodb.net/toDoList?retryWrites=true&w=majority', { useNewUrlParser: true })
+const Conn = (url, user, pass, data) => {
+  mongoose.connect(`${url}/${data}`, {
+    user: user,
+    pass: pass, 
+    useNewUrlParser: true 
+    })
   .then(() => console.log('Conexão com MongoDB executada com sucesso!'))
   .catch((err) => console.error(`Erro na conexão com o banco: ${err}`));
 }
--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+if(process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
 const express = require('express');
 const cors = require('cors');
 const Conn = require('./conn/conn');
@@ -7,9 +11,14 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-Conn();
+const db_url = process.env.DB_URL;
+const db_user = process.env.DB_USER;
+const db_pass = process.env.DB_PASS;
+const db_data = process.env.DB_DATA;
+
+Conn(db_url, db_user, db_pass, db_data);
 
 const port = 3000;
-app.listen(port, () => {
+app.listen(process.env.PORT | port, () => {
   console.log(`Servidor inicializado na porta ${port}`);
 });
